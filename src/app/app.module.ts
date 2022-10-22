72dccd02259a10b737720e9bdef4b2be91f69c05
Julien Stuyck <stuyck@gmail.com>
2022-10-22 21:25:21 +0200

    Add Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    MatButtonModule
+    MatButtonModule,
+    BrowserAnimationsModule
   ],
   exports: [
     MatButtonModule
